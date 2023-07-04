929f97ec9a887d4ad458e28c1efb11780dbb9399
pujangga123 <rush_x@yahoo.com>
2023-07-04 22:30:46 +0700

    update tokobuku.sql

--- src/tokobuku.sql
+++ src/tokobuku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2023 at 06:31 PM
+-- Generation Time: Jul 04, 2023 at 05:29 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -12,15 +12,11 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `tokobuku`
 --
+CREATE DATABASE IF NOT EXISTS `tokobuku` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tokobuku`;
 
 -- --------------------------------------------------------
 
@@ -28,6 +24,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `barang`
 --
 
+DROP TABLE IF EXISTS `barang`;
 CREATE TABLE `barang` (
   `kode` varchar(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
@@ -41,7 +38,8 @@ CREATE TABLE `barang` (
 
 INSERT INTO `barang` (`kode`, `nama`, `jenis`, `harga`) VALUES
 ('001', 'Buku Tulis', 'buku', 10000),
-('002', 'Pensil 2B', 'atk', 3000);
+('002', 'Pensil 2B', 'atk', 3000),
+('003', 'Chiki', 'snack', 5000);
 
 -- --------------------------------------------------------
 
@@ -49,9 +47,10 @@ INSERT INTO `barang` (`kode`, `nama`, `jenis`, `harga`) VALUES
 -- Table structure for table `jualdetail`
 --
 
+DROP TABLE IF EXISTS `jualdetail`;
 CREATE TABLE `jualdetail` (
   `id` int(11) NOT NULL,
-  `jualmasterNomor` varchar(10) NOT NULL,
+  `jualNomor` int(11) NOT NULL,
   `barangKode` varchar(10) NOT NULL,
   `qty` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
@@ -64,8 +63,9 @@ CREATE TABLE `jualdetail` (
 -- Table structure for table `jualmaster`
 --
 
+DROP TABLE IF EXISTS `jualmaster`;
 CREATE TABLE `jualmaster` (
-  `nomor` varchar(10) NOT NULL,
+  `nomor` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `konsumenId` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -76,6 +76,7 @@ CREATE TABLE `jualmaster` (
 -- Table structure for table `konsumen`
 --
 
+DROP TABLE IF EXISTS `konsumen`;
 CREATE TABLE `konsumen` (
   `id` varchar(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
@@ -99,6 +100,12 @@ ALTER TABLE `jualdetail`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `jualmaster`
+--
+ALTER TABLE `jualmaster`
+  ADD PRIMARY KEY (`nomor`);
+
+--
 -- Indexes for table `konsumen`
 --
 ALTER TABLE `konsumen`
@@ -113,8 +120,10 @@ ALTER TABLE `konsumen`
 --
 ALTER TABLE `jualdetail`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT for table `jualmaster`
+--
+ALTER TABLE `jualmaster`
+  MODIFY `nomor` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
